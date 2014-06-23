6dd46357321e7276438ad806ce377b5b3e3474b4
Chris Seguinot <christophe.seguinot@numericable.fr>
2014-06-23 14:07:14 +0200

    June 23 2014

---
+++ administrator/components/com_jtg/sql/upgrade/0.8.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `#__jtg_config` CHANGE `gid` `gid` VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'a:0:{}';
+ALTER TABLE `#__jtg_config` CHANGE `comment_who` `comment_who` VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'a:0:{}';
