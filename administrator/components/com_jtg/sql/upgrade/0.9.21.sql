a9299a354fcff759e1e75e43b3b1e11bf1bd37fe
Christophe <christophe@jtrackgallery.net>
2015-11-30 00:02:54 +0100

    O.9.21 + translation fix (German/Spanish/French)

--- administrator/components/com_jtg/sql/upgrade/0.9.21.sql
+++ administrator/components/com_jtg/sql/upgrade/0.9.21.sql
@@ -1,1 +1,7 @@
 ALTER TABLE `#__jtg_files` ADD `isroundtrip` TINYINT(1) NOT NULL DEFAULT '0' AFTER `iscache`;
+
+ALTER TABLE `#__jtg_cats` ADD `default_map` INT(2) NULL DEFAULT NULL AFTER `published`, ADD `default_overlays` TEXT 
+NULL DEFAULT NULL AFTER `default_map`;
+
+ALTER TABLE `#__jtg_files` ADD `default_map` INT(2) NULL DEFAULT NULL AFTER `published`, ADD `default_overlays` TEXT 
+NULL DEFAULT NULL;
