da66d72653b3e70cce564391cd39682ebc990353
Christophe <christophe@jtrackgallery.net>
2015-12-13 22:37:58 +0100

    Jtg_Plugin working on J!2.5 and 3.x (missing translation)

--- plugins/content/plg_jtrackgallery/plg_jtrackgallery.php
+++ plugins/content/plg_jtrackgallery/plg_jtrackgallery.php
@@ -19,8 +19,6 @@ if (version_compare ( JVERSION, '1.6.0', 'ge' )) {
 }
 class plgContentPlg_jtrackgallery extends JPlugin {
 
-
-
 	function onContentPrepare($context, &$row, &$params, $page = 0) {
 		$this->renderJtrackGalleryPlugin ( $row, $params, $page = 0 );
 	}
@@ -46,7 +44,12 @@ class plgContentPlg_jtrackgallery extends JPlugin {
 		if (JPluginHelper::isEnabled ( 'content', $plg_name ) == false)
 			return;
 
-			// Bail out if the page format is not what we want
+		// Check
+		if (!JComponentHelper::isEnabled('com_jtg', true)) {
+			return JError::raiseError(JText::_('PLG_JTG_COM_JTG_NOT_INSTALLED'));
+		}
+
+		// Bail out if the page format is not what we want
 		$allowedFormats = array (
 				'',
 				'html'
@@ -82,15 +85,13 @@ class plgContentPlg_jtrackgallery extends JPlugin {
 		// Get plugin info
 		$plugin = JPluginHelper::getPlugin ( 'content', $plg_name );
 
-		// Control external parameters and set variable for controlling plugin layout within modules
-
-
 		// ----------------------------------- Prepare the output -----------------------------------
 
 		// Process plugin tags
 		if (preg_match_all ( $regex, $row->text, $matches, PREG_PATTERN_ORDER ) > 0) {
 
-			// start the replace loop
+			// Start the replace loop
+			$map_count = 0;
 			foreach ( $matches [0] as $key => $match ) {
 
 				$plg_params = array (
@@ -105,7 +106,7 @@ class plgContentPlg_jtrackgallery extends JPlugin {
 					$plg_params [trim ( $temp [0] )] = trim ( $temp [1] );
 				}
 				$plg_params ['id'] = ( int ) $plg_params ['id'];
-				$warningtext = '<br>-- id=' . ($plg_params ['id'] ? $plg_params ['id'] : 'null') . ' <br>--gpxfilename=' . ($plg_params ['gpxfilename'] ? $plg_params ['gpxfilename'] : '');
+				$warningtext = ' id=' . ($plg_params ['id'] ? $plg_params ['id'] : 'null') . ' gpxfilename=' . ($plg_params ['gpxfilename'] ? $plg_params ['gpxfilename'] : '') ;
 
 				if ((! $plg_params ['id'] > 0) and (! $plg_params ['gpxfilename'])) {
 					JError::raiseNotice ( '', JText::_ ( 'PLG_JTG_TRACK_NOT_SPECIFIED' ) . "()" );
@@ -127,38 +128,110 @@ class plgContentPlg_jtrackgallery extends JPlugin {
 					}
 					else
 					{
-						JError::raiseNotice ( '', JText::_ ( 'PLG_JTG_TRACK_NOT_FOUND' ) . "($warningtext)" );
+						JError::raiseNotice ( '', JText::_ ( 'PLG_JTG_TRACK_NOT_FOUND' ) . " ($warningtext)" );
 						$plg_params ['id'] = 0;
 					}
 				}
 
-				if ($plg_params ['id'] > 0) {
+				$plg_html = $plg_copyrights_start;
+
+				if ($plg_params ['id'] > 0)
+				{
 					// Generate the html code for the map
-					$plg_html = $plg_copyrights_start;
-					$plg_html .= $this->rendermap($plg_params);
-					$plg_html .= $plg_copyrights_end;
-					// Do the replace
-					$row->text = str_replace ( $match, $plg_html, $row->text );
+					$map_count += 1;
+					if ($map_count <2)
+					{
+						$plg_html .= $this->rendermap($plg_params);
+					}
+					else
+					{
+						JError::raiseNotice ( '', JText::_ ( 'PLG_JTG_CANT_RENDER_TRACKS' ));
+						$plg_html .= JText::_ ( 'PLG_JTG_CANT_RENDER_TRACKS' );
+					}
+
 				} else {
-					// Remove tag?
-					$plg_html = $plg_copyrights_start;
-					$plg_html .= "<br>--ERROR PLG_JTRACKGALLERY--<br>" .
-						' id=' . ($plg_params['id'] ? $plg_params['id'] : 'null') .
-						' gpxfilename=' . ($plg_params ['gpxfilename'] ? $plg_params ['gpxfilename'] : '');
-					$plg_html .= $plg_copyrights_end;
-
-					// Do the replace
-					$row->text = str_replace ( $match, $plg_html, $row->text );
+					$plg_html .= JText::_ ( 'PLG_JTG_TRACK_NOT_FOUND' ) . " ($warningtext)" ;
 				}
+				$plg_html .= $plg_copyrights_end;
+				// Do the replace
+				$row->text = str_replace ( $match, $plg_html, $row->text );
 
-			} // end foreach
+			}
 
-		} // end if
-	} // END FUNCTION
+		}
+	}
+
 	private function rendermap($plg_params)
 	{
-		$map="<br>THIS IS A MAP<br>";
-		return $map;
+		$document = JFactory::getDocument();
+		$document->addStyleSheet(JUri::base(true) . '/components/com_jtg/template.css');
+		$document->addStyleSheet('http://dev.openlayers.org/theme/default/style.css');
+
+		// Add jtg_map stylesheet
+		require_once JPATH_SITE . '/components/com_jtg/helpers/helper.php';
+		$cfg = JtgHelper::getConfig();
+		$tmpl = ($cfg->template <> "") ? $cfg->template : 'default';
+		$document->addStyleSheet(JUri::root(true) . '/components/com_jtg/assets/template/' . $tmpl . '/jtg_map_style.css');
+		$map = "";
+
+		// Load english language file for 'com_jtg' component then override with current language file
+		JFactory::getLanguage()->load('com_jtg_common', JPATH_SITE . '/components/com_jtg',	null, true);
+
+		// Com_jtg_additional language files are in /images/jtrackgallery/language folder
+		JFactory::getLanguage()->load('com_jtg_additional', JPATH_SITE . '/images/jtrackgallery',	null, true);
+
+
+		// Edit file
+		$cache = JFactory::getCache('plg_jtrackgallery');
+		$params = JComponentHelper::getParams('com_jtg');
+
+		require_once JPATH_SITE . '/components/com_jtg/models/files.php';
+		$model = JModelLegacy::getInstance( 'Files', 'JtgModel' );
+		$track = $cache->get(array($model, 'getFile'), array($plg_params['id']));
+		$document = JFactory::getDocument();
+		require_once JPATH_SITE . '/components/com_jtg/helpers/gpsClass.php';
+		$document->addScript('http://www.openlayers.org/api/OpenLayers.js');
+		$document->addScript( JUri::root(true) . '/components/com_jtg/assets/js/fullscreen.js');
+		$document->addScript('http://www.openstreetmap.org/openlayers/OpenStreetMap.js');
+		$document->addScript( JUri::root(true) . '/components/com_jtg/assets/js/jtg.js');
+		$file = JPATH_SITE . '/images/jtrackgallery/uploaded_tracks/' . $track->file;
+		$gpsData = new GpsDataClass($cfg->unit);
+		$gpsData = $cache->get(array ( $gpsData, 'loadFileAndData' ), array ($file, $track->file ), $cfg->unit);
+
+		if ($gpsData->displayErrors())
+		{
+			$map = "";
+		}
+		else
+		{
+			$map = $cache->get(array ( $gpsData, 'writeTrackOL' ), array ( $track, $params ));
+			$map.= '<style type="text/css">
+
+.olButton::before{
+	display: none;
+}
+#jtg_map img{
+	max-width: none; /* joomla3 max-width=100% breaks popups*/
+}
+
+/* Fix Bootstrap-Openlayers issue */
+.olMap img { max-width: none !important;
+}
+
+img.olTileImage {
+	max-width: none !important;
+}
+</style>';
+		$map_width = isset ($plg_params ['map_width'])? $plg_params ['map_width']: $cfg->map_width;
+		$map_height = isset ($plg_params ['map_height'])? $plg_params ['map_height']: $cfg->map_height;
+
+		$map .= ("\n<div id=\"jtg_map\"  align=\"center\" class=\"olMap\" ");
+		$map .= ("style=\"width: $map_width; height: $map_height; background-color:#EEE; vertical-align:middle;\" >");
+		$map .= ("\n<script>slippymap_init();</script>");
+		$map .= ("\n</div>");
 	}
+
+	return $map;
+}
 
 } // END CLASS
