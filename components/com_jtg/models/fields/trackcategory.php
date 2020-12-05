0add3fdf0759b837aa7e7a7862ad50fc5113b0de
Marco van Leeuwen <mastervanleeuwen@gmail.com>
2020-12-05 20:31:37 +0100

    Add license info

--- components/com_jtg/models/fields/trackcategory.php
+++ components/com_jtg/models/fields/trackcategory.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * @component  J!Track Gallery (jtg) for Joomla! 2.5 and 3.x
+ *
+ *
+ * @package     Comjtg
+ * @subpackage  Frontend
+ * @author      Christophe Seguinot <christophe@jtrackgallery.net>
+ * @author      Pfister Michael, JoomGPStracks <info@mp-development.de>
+ * @author      Christian Knorr, InJooOSM  <christianknorr@users.sourceforge.net>
+ * @copyright   2015 J!TrackGallery, InJooosm and joomGPStracks teams
+ *
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3
+ * @link        http://jtrackgallery.net/
+ *
+ */
 
 defined('JPATH_BASE') or die;
 
