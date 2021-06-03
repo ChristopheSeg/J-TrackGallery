99a298e678f514ca2a2fe900356e9932515385e4
Marco van Leeuwen <mastervanleeuwen@gmail.com>
2021-06-03 23:46:52 +0200

    Change version to 0.9.32 to update comment table with uid field

--- administrator/components/com_jtg/sql/upgrade/0.9.31.sql
+++ administrator/components/com_jtg/sql/upgrade/0.9.31.sql
@@ -2,9 +2,3 @@ ALTER TABLE `#__jtg_files`
 MODIFY `title` varchar(127);
 ALTER TABLE `#__jtg_files`
 MODIFY `file` varchar(127);
-ALTER TABLE `#__jtg_comments`
-MODIFY `email` varchar(80);
-ALTER TABLE `#__jtg_comments`
-MODIFY `homepage` varchar(255);
-ALTER TABLE `#__jtg_comments`
-ADD `uid` int(10);
